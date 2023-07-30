f7b28c3bde77bb5c3edc3681e20238231d593585
wiliansaugusto <wiliansaugusto@gmail.com>
2023-07-30 18:09:53 -0300

    V1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'sites';
+  showValueDiv:boolean = true;
 
+  ngOnInit(){
+    if (window.sessionStorage.getItem("popup-exibida")=="1"){
+      this.showValueDiv = false;
+
+    }
+  }
+  setPopUp(){
+    window.sessionStorage.setItem("popup-exibida", "1");
+    console.warn("setPop"+window.sessionStorage.getItem("popup-exibida"));
+    this.showValueDiv = false;
+
+  }
 }
