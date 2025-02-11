da18bd1bc9a8e2b99a8c2bbc051597cf4c5da4ab
wiliansaugusto <wiliansaugusto@gmail.com>
2025-02-11 08:19:08 -0300

    update em Og:cards

--- src/app/views/willians/willians.component.ts
+++ src/app/views/willians/willians.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
@@ -6,20 +6,18 @@ import { Meta, Title } from '@angular/platform-browser';
   templateUrl: './willians.component.html',
   styleUrls: ['./willians.component.css']
 })
-export class WilliansComponent {
-  constructor(private meta: Meta, private title: Title) {
-    title.setTitle('Achei Digital - Willians Augusto');
-    meta.addTags([
-      { name: 'author', content: 'Willians Augusto' },
-      { name: 'keywords', content: 'Desenvolvedor de Software, pós graduado em Java, Desenvolvedor Angular, Desenvolvedor, ' },
-      { name: 'description', content: 'Conheça nosso colaborador Willians Augusto, Analista de Sistema pós graduado em Java' },
-      { name: 'robots', content: 'index, follow' },
-      { name: 'og:title', content: 'Achei Digital - Willians Augusto' },
-      { name: 'og:description', content: 'Conheça nossos time de sucesso!' },
-      { name: 'og:image', content: 'https://sites.achei.digital/assets/img/foto3X4Willians.jpg' },
-      { name: 'og:url', content: 'https://www.sites.acheidigital.com.br/williansaugusto' }
-    ]);
-
+export class WilliansComponent implements OnInit {
+  constructor(private meta: Meta, private title: Title) { }
+  ngOnInit() {
+    this.title.setTitle('Achei Digital - Willians Augusto');
+    this.meta.updateTag({ property: 'author', content: 'Willians Augusto ' });
+    this.meta.updateTag({ property: 'keywords', content: 'Desenvolvedor de Software, pós graduado em Java, Desenvolvedor Angular, Desenvolvedor, ' });
+    this.meta.updateTag({ property: 'description', content: 'Conheça nosso colaborador Willians Augusto, Analista de Sistema pós graduado em Java' });
+    this.meta.updateTag({ property: 'robots', content: 'index, follow' });
+    this.meta.updateTag({ property: 'og:title', content: 'Willians Augusto' });
+    this.meta.updateTag({ property: 'og:description', content: 'Conheça nossos time de sucesso!' });
+    this.meta.updateTag({ property: 'og:image', content: 'https://sites.achei.digital/assets/img/foto3X4Willians.jpg' });
+    this.meta.updateTag({ property: 'og:url', content: 'https://www.sites.acheidigital.com.br/williansaugusto' });
+    this.meta.updateTag({ property: "og:site_name", content: "Willians Augusto - Achei Digital" });
   }
-
 }
