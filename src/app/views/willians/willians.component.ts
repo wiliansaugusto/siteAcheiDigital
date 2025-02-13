7aa2fbdb5bda17204c514bd79beebdf71f139316
wiliansaugusto <wiliansaugusto@gmail.com>
2025-02-13 10:45:33 -0300

    Atualização Site Willians

--- src/app/views/willians/willians.component.ts
+++ src/app/views/willians/willians.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
@@ -6,13 +6,26 @@ import { Meta, Title } from '@angular/platform-browser';
   templateUrl: './willians.component.html',
   styleUrls: ['./willians.component.css']
 })
-export class WilliansComponent {
+export class WilliansComponent implements OnInit {
+  idadePedro: any = this.calcularIdade();
   constructor(private meta: Meta, private title: Title) {
     title.setTitle('Willians Augusto - Achei Digital');
     meta.updateTag({ property: 'author', content: 'Willians Augusto' });
     meta.updateTag({ name: 'keywords', content: 'Desenvolvedor de Software, pós graduado em Java, Desenvolvedor Angular, Desenvolvedor, ' });
     meta.updateTag({ name: 'description', content: 'Conheça nosso colaborador Willians Augusto, Analista de Sistema pós graduado em Java' });
     meta.updateTag({ name: 'robots', content: 'index, follow' });
+  }
+  ngOnInit(): void {
+    window.scrollTo({
+      top: 140, behavior: 'smooth'
+    });
+  }
+  calcularIdade() {
+    const data = new Date("2007-07-18 ");
+    const dataAtual = new Date();
+    const diferenca = Math.abs(dataAtual.getTime() - data.getTime());
+    const idade = Math.floor(diferenca / (1000 * 3600 * 24) / 365.25);
+    return idade;
   }
 
 }
